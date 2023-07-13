c4f3027567d8e8dfc9e161b2c45629bd344f6f74
Michael Voorhees <michaelv@unity3d.com>
2023-07-13 14:07:20 -0400

    Remove NETCOREAPP behavior difference in debugger attribute removal tests

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Attributes.Debugger/DebuggerDisplayAttributeOnTypeWithNonExistentMethod.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Attributes.Debugger/DebuggerDisplayAttributeOnTypeWithNonExistentMethod.cs
@@ -2,18 +2,9 @@
 
 using System.Diagnostics;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
-using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.Attributes.Debugger
 {
-#if !NETCOREAPP
-	[SetupLinkerTrimMode ("link")]
-
-	// Can be removed once this bug is fixed https://bugzilla.xamarin.com/show_bug.cgi?id=58168
-	[SkipPeVerify (SkipPeVerifyForToolchian.Pedump)]
-
-	[KeptMemberInAssembly (PlatformAssemblies.CoreLib, typeof (DebuggerDisplayAttribute), ".ctor(System.String)")]
-#endif
 	public class DebuggerDisplayAttributeOnTypeWithNonExistentMethod
 	{
 		public static void Main ()
