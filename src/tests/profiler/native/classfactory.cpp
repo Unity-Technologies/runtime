37e2f693a793a7533251f5801a113f6c68f1f7bd
Simon Ferquel <simon.ferquel@unity3d.com>
2025-04-03 10:36:15 +0200

    Test for dynamic assignment of COR_PRF_DISABLE_OPTIMIZATIONS and COR_PRF_DISABLE_INLINING

--- src/tests/profiler/native/classfactory.cpp
+++ src/tests/profiler/native/classfactory.cpp
@@ -21,6 +21,7 @@
 #include "moduleload/moduleload.h"
 #include "assemblyprofiler/assemblyprofiler.h"
 #include "classload/classload.h"
+#include "dynamicjitoptimization/dynamicjitoptimization.h"
 
 ClassFactory::ClassFactory(REFCLSID clsid) : refCount(0), clsid(clsid)
 {
@@ -143,6 +144,10 @@ HRESULT STDMETHODCALLTYPE ClassFactory::CreateInstance(IUnknown *pUnkOuter, REFI
     else if (clsid == ClassLoad::GetClsid())
     {
         profiler = new ClassLoad();
+    }
+    else if (clsid == DynamicJitOptimizations::GetClsid())
+    {
+        profiler = new DynamicJitOptimizations();
     }
     else
     {
